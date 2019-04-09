12f36e578d05fc22148702d1ebac2f450a13056d
motomura <43534770+motomuratakahiro@users.noreply.github.com>
2019-04-10 06:25:31 +0900

    GoogleSpreadに予約

---
+++ firebaseFulfillment/firebase/functions/index.js
@@ -0,0 +1,99 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+//httpの利用を宣言する
+const https = require('https');
+ 
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  
+  function namae(agent) {
+    return new Promise((resolve, reject) => {
+      let context = agent.getContext(`hinichi-followup`);
+      
+                   
+      let date = context[`parameters`][`date`];
+      let name = agent.parameters[`any`]; 
+      let url = 'https://script.google.com/macros/s/AKfycbzGLMY1kIsI84ph7PpzKTkJlbylbeiZnDhTnpPMY64EWK9MTp8/exec?date=' + encodeURIComponent(date.substring(1,10)) + '&name=' + encodeURIComponent(name);
+      
+      console.log(`request:` + url);
+      //httpのリクエストを送信
+        let req = https.get(url, (res) => {
+          let chunk = '';
+          //読み込み中の処理
+          res.on('data', (c) => {
+            chunk += c;
+          });
+          
+          //読み込み完了時の処理
+          res.on('end', () => {
+            //処理終了
+            resolve();
+          });
+        });
+        
+        //エラー時の処理
+        req.on('error', (e) => {
+          console.error(`エラー： ${e.message}`);
+        });
+    });
+  }
+  
+  
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  // // Uncomment and edit to make your own intent handler
+  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function yourFunctionHandler(agent) {
+  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+  //   agent.add(new Card({
+  //       title: `Title: this is a card title`,
+  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+  //       buttonText: 'This is a button',
+  //       buttonUrl: 'https://assistant.google.com/'
+  //     })
+  //   );
+  //   agent.add(new Suggestion(`Quick Reply`));
+  //   agent.add(new Suggestion(`Suggestion`));
+  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  // }
+
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function googleAssistantHandler(agent) {
+  //   let conv = agent.conv(); // Get Actions on Google library conv instance
+  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+  // }
+  // // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
+  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('namae', namae);
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
